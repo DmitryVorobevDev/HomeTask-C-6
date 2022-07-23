0b7d4c26e76e02b6d5950d2d054104b1abc1a348
Dmitry <vorobyev.d.o@gmail.com>
2022-07-23 19:03:21 +0300

    Add example 001

---
+++ Example001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void GetResult (int count)
+{
+    
+    string valueFromConsole = Console.ReadLine ();
+    if (int.TryParse(valueFromConsole, out int number))
+    {
+        if (number > 0)
+        {
+        count ++;
+        GetResult (count);
+        }
+        else GetResult(count);
+    }
+    else Console.WriteLine("Количество положительных чисел введеных пользователем: " + count);
+}
+
+Console.WriteLine ("Введите числа, програма посчитает количество положительных чисел введеных вами.");
+Console.WriteLine ("Введение любых иных значений (кроме числа) прекращает ввод");
+GetResult(0);
